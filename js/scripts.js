60fb435b097e19697d5cf9d25e29046fd4e6ab24
Adarnok <Adarnok@poczta.fm>
2018-08-22 23:48:08 +0200

    pierwszy

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,41 @@
 'use strict';
-function calc(b) {
-    const res = document.querySelector('#res');
-    const liczba = b.value;
-    if (b.value == "=") {
-        res.value = eval(res.value);
+(function(){
+    
+    class Kalkulator {
+        constructor(result) {
+            this._guziczki = document.querySelectorAll('input[type="button"]');
+            this._resPlace = document.querySelector('#' + result);
+            console.log(this._guziczki);
+            console.log(this._resultPlace);
+            this._guziki(this._guziczki);
+        }
+        // Kalkulator.prototype._guziki = function(guzik){
+        //     guzik.forEach(function(elem, i){
+        //         elem.addEventListener('click', this._calc.bind(this), this);
+        //     })
+        // }
+        _guziki(guzik) {
+            guzik.forEach(this._click, this);
+        }
+        _click(elem, i) {
+            elem.addEventListener('click', this._calc.bind(this));
+        }
+        _calc(button) {
+            const buttonValue = button.target.value;
+            if (buttonValue === 'C') {
+                this._resPlace.value = '';
+            }
+            else if (buttonValue === '=') {
+                this._resPlace.value = eval(this._resPlace.value);
+            }
+            else {
+                this._resPlace.value += buttonValue;
+            }
+        }
     }
-    else
-    if (b) {
-        res.value += liczba;
-    }   
-    if (b.value == "C") {
-        res.value = '';
-    }
-}
+
+
+    
+    new Kalkulator('res');
+    // new Kalkulator();
+})()
